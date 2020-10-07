e9d4e59baa4d39a8d767e760ec030e26d146495d
simshengqin <54699127+simshengqin@users.noreply.github.com>
2020-10-08 01:27:13 +0800

    Demo

--- retrieve_message_leftbar.php
+++ retrieve_message_leftbar.php
@@ -33,7 +33,7 @@ if(isset($_GET['user_id']) && isset($_GET['user_type'])){
             $from_type = $message->get_to_type();                            
         } 
         if ($from_type == "user") {
-            $individual_message["from_name"] = "User"//ucfirst($userDAO->retrieve_user($from_id)->get_name());
+            $individual_message["from_name"] = "User";//ucfirst($userDAO->retrieve_user($from_id)->get_name());
             $individual_message["from_image"] = "images/profile_picture/user/$from_id.png";
             //Use the default picture if the image does not exist
             if (!file_exists($individual_message["from_image"])) {
@@ -43,7 +43,7 @@ if(isset($_GET['user_id']) && isset($_GET['user_type'])){
         }
         else if ($from_type == "company") {
             
-            $individual_message["from_name"] = "Company"u//cfirst($companyDAO->retrieve_company($from_id)->get_name());
+            $individual_message["from_name"] = "Company";//cfirst($companyDAO->retrieve_company($from_id)->get_name());
             $individual_message["from_image"] = "images/profile_picture/company/$from_id.png";
             //Use the default picture if the image does not exist
             if (!file_exists($individual_message["from_image"])) {
